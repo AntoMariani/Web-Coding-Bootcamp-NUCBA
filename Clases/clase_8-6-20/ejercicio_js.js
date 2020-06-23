f63a4967ad1aa81a1676062799e37a81ff5c8829
Antonella Mariani <antomaariani@gmail.com>
2020-06-23 01:50:19 -0300

    agregado de clases al repositorio

---
+++ Clases/clase_8-6-20/ejercicio_js.js
@@ -0,0 +1,20 @@
+var resp1 = prompt('¿En que año finalizó la Segunda Guerra Mundial?')
+if (resp1 == 1945) {
+    alert('Respuesta correcta')
+} else { 
+    alert('Respuesta incorrecta')
+    }
+
+var resp2 = prompt('¿Cuantos campeonatos de Formula 1 ganó Michael Schumacher?')
+if (resp2 == 7) {
+    alert('Respuesta correcta')
+} else { 
+    alert('Respuesta incorrecta')
+    }
+
+    var resp3 = prompt('¿En que año jugó Argentina su final de Mundial mas reciente?')
+if (resp3 == 2014) {
+    alert('Respuesta correcta')
+} else { 
+    alert('Respuesta incorrecta')
+    }
---
+++ Clases/clase_8-6-20/ejercicio_js_2.js
@@ -0,0 +1,19 @@
+var opcion= prompt("Ingrese la operación a realizar:")
+
+switch(opcion){
+    case 'Ver saldo de tarjeta':
+        alert("Opcion correcta")
+    break
+    case 'Pagar tarjeta':
+        alert("Opcion correcta")
+    break
+    case 'Ver proveedor de tarjeta':
+        alert("Opcion correcta")
+    break
+    case 'Ver dueño de tarjeta':
+        alert("Opcion correcta")
+    break
+    default:
+        alert("Opcion incorrecta")
+
+}
