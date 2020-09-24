bf571b82aca0b8350cba8ed7368614e0509f6877
Antonella Mariani <antomaariani@gmail.com>
2020-09-24 00:37:09 -0300

    agregado cambios

--- webPeriodisticaJS/script.js
+++ webPeriodisticaJS/script.js
@@ -1,5 +1,5 @@
 class noticia {
-    constructor(titulo, seccion, copete, leerMas, href, fecha, imgSrc) {
+    constructor(titulo, copete, seccion,  leerMas, href, fecha, imgSrc) {
         this.titulo = titulo;
         this.copete = copete;
         this.seccion = seccion;
@@ -43,6 +43,7 @@ let noticia3 = new noticia (
 
 let listaNoticias = [noticia1, noticia2, noticia3]
 
+
 /*{ <div class="card">
         <div class="imgcard">
           <img src="./Noticias/img/thelastofus.png" alt="" />
@@ -65,6 +66,8 @@ let listaNoticias = [noticia1, noticia2, noticia3]
         </div>
 </div> }*/
 
+
+
 function crearCard(noticia) {
 
     let contenedorNoticias = document.getElementById("contenedor-noticias") //agarrra la direccion de contenedorNoticias
@@ -78,17 +81,61 @@ function crearCard(noticia) {
     divImg.classList.add("imgcard") //agrega la clase a la div de imagen
     div_card.appendChild(divImg) //appendea la div imagen a la div card
     
-    //IMAGEN DENTRO DE LA DIV
-    let img_prev = document.createElement("img") //crea el elemento imagen
-    img_prev.src = "./Noticias/img/" + noticia.imgSrc //crea el link imagen y lo coloca
-    divImg.appendChild(img_prev) //coloca la imagen dentro de div imagen
+    // //IMAGEN DENTRO DE LA DIV
+    // let imgNoticia = document.createElement("img") //crea el elemento imagen
+    // imgNoticia.src = "./Noticias/img/" + noticia.imgSrc //crea el link imagen y lo coloca
+    // divImg.appendChild(imgNoticia) //coloca la imagen dentro de div imagen
+
+    //IMAGEN DENTRO DE LA DIV DESDE UNA API RANDOM
+
+    fetch('https://source.unsplash.com/user/erondu/353x200', {}) //LLAMADO A UN SERVICIO O API QUE NOS TRAE UNA IMAGEN RANDOM
+    .then(function (response) {
+        let contenedorNoticias = document.getElementById("contenedor-noticias")
+        let imgNoticia = document.createElement("img") //crea el elemento imagen
+        imgNoticia.src = response.url //crea el link imagen y lo coloca
+        divImg.appendChild(imgNoticia) //coloca la imagen dentro de div imagen
+    })
 
     //AGREGAR EL CONTENIDO DE LA CARD
     let divContenido = document.createElement("div")
     divContenido.classList.add("contenidocard")
     div_card.appendChild(divContenido)
 
-    
+    //AGREGAR FECHA
+    let fechaNoticia = document.createElement("p");
+    fechaNoticia.classList.add("fechacard");
+    fechaNoticia.innerHTML = noticia.fecha;
+    divContenido.appendChild(fechaNoticia);
+
+    //AGREGAR TITULO
+    let tituloNoticia = document.createElement("h2");
+    tituloNoticia.innerHTML = noticia.titulo;
+    divContenido.appendChild(tituloNoticia);
+
+    //AGREGAR SECCION DE LA NOTICIA
+    let seccionNoticia = document.createElement("p");
+    seccionNoticia.classList.add("seccioncard");
+    seccionNoticia.innerHTML = noticia.seccion;
+    divContenido.appendChild(seccionNoticia);
+
+    //AGREGAR COPETE DE LA NOTICIA
+    let copeteNoticia = document.createElement("p");
+    copeteNoticia.classList.add("copetecard");
+    copeteNoticia.innerHTML = noticia.copete;
+    divContenido.appendChild(copeteNoticia);
+
+    //AGREGAR LINK LEER MÁS
+
+    let anchorNoticia = document.createElement("a");
+        //crear el nodo de texto y appendearlo 
+    let textAnchorNoticia = document.createTextNode("Leer mas")
+    anchorNoticia.appendChild(textAnchorNoticia)
+
+    anchorNoticia.href = './Noticias' + noticia.href
+    anchorNoticia.target = '_blank'
+    divContenido.appendChild(anchorNoticia)
+
+    //AGREGAMOS CADA CARD A LA PAGINA
     contenedorNoticias.appendChild(div_card) //appendea la div card al contenedor noticias
 }
 
